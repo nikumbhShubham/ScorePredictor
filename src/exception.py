65dde11db393c1d2e8f112c464a64d15bfe19209
nikumbhShubham <shubhamnikumbh19@gmail.com>
2025-11-25 20:34:28 +0530

    dataset and model training

--- src/exception.py
+++ src/exception.py
@@ -14,12 +14,9 @@ def error_message_detail(error, error_detail: sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        full_message=error_message_detail(error_message, error_detail=error_detail)
-        self.error_message=full_message
-        logging.error(full_message)
+        self.error_message=error_message_detail(error_message, error_detail=error_detail)
+
         
-    
-    
     def __str__(self):
         return self.error_message  
     
