cf8caf84e26de2d674c9b321f51e85698cde93dd
nikumbhShubham <shubhamnikumbh19@gmail.com>
2025-11-27 16:38:39 +0530

    hyperparameter tuning for all models

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.logger import logging
 import logging
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -23,13 +24,20 @@ def save_object(file_path:str,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models:dict):
+def evaluate_models(X_train,y_train,X_test,y_test,models:dict,params):
     try:
         report={}
         
         for i in range(len(models)):
-            model=list(models.values())[i]
             
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            # model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             
             y_test_pred=model.predict(X_test)
